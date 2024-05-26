a8940d33495a3907e309714a955b0cee3ccf8dc9
Ricardo Barbosa <ricardobarbosacosta156@gmail.com>
2024-05-26 15:34:48 -0300

    salvando minha aplicação em java

---
+++ src/main/java/br/com/ricardao/todolist/controller/MinhaPrimeiraConntrolier.java
@@ -0,0 +1,12 @@
+package br.com.ricardao.todolist.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MinhaPrimeiraConntrolier {
+    
+    public String primeiraMensage() {
+        return null;
+
+    } 
+}   
